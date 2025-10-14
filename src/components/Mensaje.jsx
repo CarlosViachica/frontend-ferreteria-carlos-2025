16b691eda2abda957d8e3034da130fd28ebfdd3f
Carlosg66 <guevaracarloseduardo66@gmail.com>
2025-10-14 11:21:01 +0200

    Creacion de las vistas con el encabezado y su configuracion

---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,16 @@
+import  React from 'react';
+
+const Mensaje = () => {
+
+return(
+<>
+<li>Es maravillso</li>
+<li>Lo chahpt</li>
+<li>Porque me gusta</li>
+<li>one two threeplejueputa</li>
+</>
+);
+
+}
+
+export default Mensaje;
